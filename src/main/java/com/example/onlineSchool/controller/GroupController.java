35c9dc0e323b9c28ab9acb697de74ab1dd1aec75
chertrom <chertrom@fit.cvut.cz>
2021-12-31 01:55:13 +0100

    Fix names (groups, subjects)

--- src/main/java/com/example/onlineSchool/controller/GroupController.java
+++ src/main/java/com/example/onlineSchool/controller/GroupController.java
@@ -10,7 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/group")
+@RequestMapping("/groups")
 public class GroupController {
     @Autowired
     GroupService groupService;
--- src/main/java/com/example/onlineSchool/controller/SubjectController.java
+++ src/main/java/com/example/onlineSchool/controller/SubjectController.java
@@ -12,7 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/subject")
+@RequestMapping("/subjects")
 public class SubjectController {
 
     @Autowired
